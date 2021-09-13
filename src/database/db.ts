e526299cc588f501d5309e6853f68e79274b7637
Ronak Jain <contact@ronakjain.com>
2021-09-13 16:50:38 +0530

    Init commit

---
+++ src/database/db.ts
@@ -0,0 +1,16 @@
+import * as process from 'process'
+import * as AWS from "aws-sdk";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+
+const createDynamoDBClient = (): DocumentClient => {
+  if (process.env.IS_OFFLINE) {
+    return new AWS.DynamoDB.DocumentClient({
+      region: "localhost",
+      endpoint: "http://localhost:5000",
+    });
+  }
+
+  return new AWS.DynamoDB.DocumentClient();
+};
+
+export default createDynamoDBClient;
---
+++ src/database/services/index.ts
@@ -0,0 +1,9 @@
+import * as process from 'process'
+import createDynamoDBClient from "../db";
+import PostService from "./postService";
+
+const { POSTS_TABLE } = process.env;
+
+const postService = new PostService(createDynamoDBClient(), POSTS_TABLE);
+
+export default postService;
